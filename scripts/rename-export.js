47e333239385a5fd7b24f468dd7b6a4b20df25e3
Ivan Storck <ivan@ivanstorck.com>
2016-09-19 20:23:31 -0700

    update script

--- scripts/rename-export.js
+++ scripts/rename-export.js
@@ -1,13 +1,14 @@
 /* global ls, mkdir, mv */
 require('shelljs/global')
+const markdownExt = 'md'
 
 function removeExt (f) {
   return f.substr(0, f.lastIndexOf('.'))
 }
 
-const files = ls('pages/articles/*.md')
+const files = ls(`pages/articles/*.${markdownExt}`)
 const dirNames = files.map(f => removeExt(f))
 mkdir('-p', dirNames)
 files.forEach(f => {
-  mv(f, `${removeExt(f)}/index.md`)
+  mv(f, `${removeExt(f)}/index.${markdownExt}`)
 })
